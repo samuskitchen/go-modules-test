4946641d6f189fc27e535dd9b3f0390623c7ffcd
Daniel De La Pava Suarez <daniel.samkit@gmail.com>
2020-01-20 15:58:32 -0500

    Sample project for GO modules

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gitlab.seven4n.com/danieldelapava/go-modules-test
+
+go 1.13
+
+require github.com/Pallinder/go-randomdata v1.1.0 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	sample "github.com/Pallinder/go-randomdata"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println(sample.SillyName())
+}
